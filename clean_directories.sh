7eabdc2ea54d8753b8e0d80877e7e8852b4eb5c8
aaccportal <aaccportal@gmail.com>
2025-03-10 13:06:20 +0300

    clean_directories update

--- clean_directories.sh
+++ clean_directories.sh
@@ -2,12 +2,12 @@
 
 # Очистка папок burstroy/api, burstroy/data, burstroy/web
 echo "Очистка папки burstroy/api..."
-rm -rf burstroy/api/*
+rm -rf /burstroy/api/*
 
 echo "Очистка папки burstroy/data..."
-rm -rf burstroy/data/*
+rm -rf /burstroy/data/*
 
 echo "Очистка папки burstroy/web..."
-rm -rf burstroy/web/*
+rm -rf /burstroy/web/*
 
 echo "Папки успешно очищены."
