2a03bdc0b709ee668c0bcc553b2e312145003a7d
aaccportal <aaccportal@gmail.com>
2025-03-25 11:29:21 +0300

    update

---
+++ nginx/setup_update_blazor_nginx4_burstroyweb.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# Создание конфигурационного файла для Nginx
+sudo bash -c 'cat > /etc/nginx/sites-available/burstroyweb' <<'EOF'
+server {
+    listen 80 default_server;
+    server_name 87.226.220.242:8082;
+
+#    location / {
+#        proxy_pass http://localhost:5000;
+#        proxy_http_version 1.1;
+#        proxy_set_header Upgrade $http_upgrade;
+#        proxy_set_header Connection "Upgrade";
+#        proxy_set_header Host $host;
+#        proxy_cache_bypass $http_upgrade;
+#        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#        proxy_set_header X-Forwarded-Proto $scheme;
+#    }
+
+#    location /ws/ {
+#        proxy_pass http://localhost:5000;
+#        proxy_http_version 1.1;
+#        proxy_set_header Upgrade $http_upgrade;
+#        proxy_set_header Connection "Upgrade";
+#        proxy_set_header Host $host;
+#        proxy_cache_bypass $http_upgrade;
+#        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+#        proxy_set_header X-Forwarded-Proto $scheme;
+#    }
+    location / {
+        proxy_pass         http://localhost:5000;
+        proxy_http_version 1.1;
+
+        proxy_set_header   Upgrade $http_upgrade;
+        proxy_set_header   Connection "upgrade";
+        proxy_set_header   Host $host;
+
+        proxy_set_header   X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header   X-Forwarded-Proto $scheme;
+
+        proxy_read_timeout 120s;
+        proxy_send_timeout 120s;
+    }
+
+}
+EOF
+
+# Создание символической ссылки на конфигурационный файл
+sudo ln -s /etc/nginx/sites-available/burstroyweb /etc/nginx/sites-enabled/
+
+# Проверка конфигурации Nginx
+sudo nginx -t
+
+# Перезапуск Nginx
+sudo systemctl restart nginx
+
+echo "Blazor приложение успешно настроено и запущено на Nginx."
---
+++ nginx/setup_update_blazor_nginx5_burstroyweb.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Создание конфигурационного файла для Nginx
+sudo bash -c 'cat > /etc/nginx/sites-available/burstroyweb' <<'EOF'
+server {
+    listen 80;
+    server_name khabarovsk.burstroy.ru;
+
+    location / {
+        proxy_pass http://localhost:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location /_blazor {
+        proxy_pass http://localhost:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+
+    location /ws/ {
+        proxy_pass http://localhost:8082;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection "Upgrade";
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}
+
+EOF
+
+# Создание символической ссылки на конфигурационный файл
+sudo ln -s /etc/nginx/sites-available/burstroyweb /etc/nginx/sites-enabled/
+
+# Проверка конфигурации Nginx
+sudo nginx -t
+
+# Перезапуск Nginx
+sudo systemctl restart nginx
+
+echo "Blazor приложение успешно настроено и запущено на Nginx."
